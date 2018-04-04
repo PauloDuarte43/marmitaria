3e4862c41322a82bad0483ccd42b1bc1ef939c13
Paulo Duarte <p.roger.antunes43@gmail.com>
2018-04-04 14:25:36 -0300

    Adicionado funcinalidades de receita e despesas

--- marmitaria/accounting/admin.py
+++ marmitaria/accounting/admin.py
@@ -5,9 +5,45 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Receita)
-admin.site.register(Despesa)
-admin.site.register(TipoDespesa)
-admin.site.register(TipoReceita)
-admin.site.register(CentroDeCusto)
-admin.site.register(TipoCentroCusto)
+
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'valor', 'tipo_despesa', 'data')
+    list_filter = ('data',)
+
+
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'valor', 'tipo_receita', 'data')
+    list_filter = ('data',)
+
+
+class CentroDeCustoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'total', 'tipo_centro_custo')
+    readonly_fields = ('total',)
+
+
+class TipoDespesaAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        """
+        return {}
+
+
+class TipoReceitaAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        """
+        return {}
+
+
+class TipoCentroCustoAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        """
+        return {}
+
+admin.site.register(Receita, ReceitaAdmin)
+admin.site.register(Despesa, DespesaAdmin)
+admin.site.register(CentroDeCusto, CentroDeCustoAdmin)
+admin.site.register(TipoDespesa, TipoDespesaAdmin)
+admin.site.register(TipoReceita, TipoReceitaAdmin)
+admin.site.register(TipoCentroCusto, TipoCentroCustoAdmin)
--- marmitaria/accounting/models.py
+++ marmitaria/accounting/models.py
@@ -2,6 +2,27 @@
 from __future__ import unicode_literals
 import datetime
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class BaseModel(object):
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        # Default implementation of from_db() (subject to change and could
+        # be replaced with super()).
+        if len(values) != len(cls._meta.concrete_fields):
+            values = list(values)
+            values.reverse()
+            values = [
+                values.pop() if f.attname in field_names else DEFERRED
+                for f in cls._meta.concrete_fields
+            ]
+        instance = cls(*values)
+        instance._state.adding = False
+        instance._state.db = db
+        # customization to store the original field values on the instance
+        instance._loaded_values = dict(zip(field_names, values))
+        return instance
 
 
 class TipoDespesa(models.Model):
@@ -29,7 +50,7 @@ class TipoCentroCusto(models.Model):
 
 
 class CentroDeCusto(models.Model):
-    total = models.DecimalField(max_digits=6, decimal_places=2)
+    total = models.DecimalField(max_digits=19, decimal_places=2, default=0)
     nome = models.CharField(max_length=40)
     descricao = models.CharField(max_length=200)
     tipo_centro_custo = models.ForeignKey(TipoCentroCusto)
@@ -38,25 +59,58 @@ class CentroDeCusto(models.Model):
         return self.nome
 
 
-class Despesa(models.Model):
-    valor = models.DecimalField(max_digits=6, decimal_places=2)
+class Despesa(BaseModel, models.Model):
+    valor = models.DecimalField(max_digits=19, decimal_places=2)
     nome = models.CharField(max_length=40)
     descricao = models.CharField(max_length=200)
     tipo_despesa = models.ForeignKey(TipoDespesa)
     centro_de_custo = models.ForeignKey(CentroDeCusto)
     data = models.DateField(default=datetime.date.today)
 
+    def clean(self):
+        #tmp = self.centro_de_custo.total - self.valor
+        #if tmp < 0:
+        #    raise ValidationError({'centro_de_custo':'Centro de custo sem fundos, selecione outro centro ou adicione nova receita'})
+        super(Despesa, self).clean()
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            centro = CentroDeCusto.objects.get(id=self.centro_de_custo.id)
+            centro.total -= self.valor
+            centro.save()
+        else:
+            if self.valor != self._loaded_values['valor']:
+                centro = CentroDeCusto.objects.get(id=self.centro_de_custo.id)
+                centro.total -= (self.valor - self._loaded_values['valor'])
+                centro.save()
+
+        super(Despesa, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return self.nome
 
 
-class Receita(models.Model):
-    valor = models.DecimalField(max_digits=6, decimal_places=2)
+class Receita(BaseModel, models.Model):
+    valor = models.DecimalField(max_digits=19, decimal_places=2)
     nome = models.CharField(max_length=40)
     descricao = models.CharField(max_length=200)
     tipo_receita = models.ForeignKey(TipoReceita)
     centro_de_custo = models.ForeignKey(CentroDeCusto)
     data = models.DateField(default=datetime.date.today)
+    
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            centro = CentroDeCusto.objects.get(id=self.centro_de_custo.id)
+            centro.total += self.valor
+            centro.save()
+        else:
+            if self.valor != self._loaded_values['valor']:
+                centro = CentroDeCusto.objects.get(id=self.centro_de_custo.id)
+                centro.total += (self._loaded_values['valor'] - self.valor)
+                centro.save()
+
+        super(Receita, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.nome
+
