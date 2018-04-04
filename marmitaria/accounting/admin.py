2a33bb4f968bd5eb8ec02bdad95f485d62b9863c
Paulo Duarte <pauloduarte@cwi.com.br>
2018-04-03 21:32:44 -0300

    Created modesl

--- marmitaria/accounting/admin.py
+++ marmitaria/accounting/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Receita)
+admin.site.register(Despesa)
+admin.site.register(TipoDespesa)
+admin.site.register(TipoReceita)
+admin.site.register(CentroDeCusto)
+admin.site.register(TipoCentroCusto)
--- marmitaria/accounting/models.py
+++ marmitaria/accounting/models.py
@@ -1,6 +1,62 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class TipoDespesa(models.Model):
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class TipoReceita(models.Model):
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class TipoCentroCusto(models.Model):
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class CentroDeCusto(models.Model):
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+    tipo_centro_custo = models.ForeignKey(TipoCentroCusto)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class Despesa(models.Model):
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+    tipo_despesa = models.ForeignKey(TipoDespesa)
+    centro_de_custo = models.ForeignKey(CentroDeCusto)
+    data = models.DateField(default=datetime.date.today)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class Receita(models.Model):
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=200)
+    tipo_receita = models.ForeignKey(TipoReceita)
+    centro_de_custo = models.ForeignKey(CentroDeCusto)
+    data = models.DateField(default=datetime.date.today)
+
+    def __unicode__(self):
+        return self.nome
