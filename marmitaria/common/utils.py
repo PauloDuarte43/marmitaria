f475b9e42627222688a0c2f17add1dafd8e27485
Paulo Duarte <pauloduarte@cwi.com.br>
2018-04-20 21:44:49 -0300

    Add report

---
+++ marmitaria/common/utils.py
@@ -0,0 +1,12 @@
+# -*- coding=utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import decimal
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
