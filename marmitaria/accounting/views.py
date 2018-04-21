98671dbf37543902e25af53e5bcf5c93f2c4b741
Paulo Duarte <pauloduarte@cwi.com.br>
2018-04-21 15:19:24 -0300

    Add resume screen

--- marmitaria/accounting/views.py
+++ marmitaria/accounting/views.py
@@ -12,12 +12,15 @@ from accounting.models import ProdutoDespesa
 from accounting.models import ProdutoReceita
 from accounting.models import Receita
 from accounting.models import Despesa
+from accounting.models import CentroDeCusto
 
 
 def range_date(request):
     if (request.method == 'POST' and request.POST.get('data-inicial') and request.POST.get('data-final')):
-        start_d = datetime.datetime.strptime(request.POST.get('data-inicial'), '%Y-%m-%d')
-        end_d = datetime.datetime.strptime(request.POST.get('data-final'), '%Y-%m-%d')
+        start_d = datetime.datetime.strptime(
+            request.POST.get('data-inicial'), '%Y-%m-%d')
+        end_d = datetime.datetime.strptime(
+            request.POST.get('data-final'), '%Y-%m-%d')
         start_day = start_d.day
         end_day = end_d.day
         start_month = start_d.month
@@ -47,19 +50,11 @@ def range_date(request):
     return data_inicial, data_final
 
 
-@login_required(login_url='/admin/login/')
-@permission_required('accounting.add_despesa', raise_exception=True)
-def index(request):
-    return render(request, 'accounting/index.html')
-
-
-@login_required(login_url='/admin/login/')
-@permission_required('accounting.add_despesa', raise_exception=True)
-def despesa(request):
-    data_inicial, data_final = range_date(request)
-    despesa = Despesa.objects.filter(data__range=(data_inicial, data_final))
-    queryset = ProdutoDespesa.objects.filter(despesa__in=despesa)
+def get_products_receita_total(data_inicial, data_final):
+    receita = Receita.objects.filter(data__range=(data_inicial, data_final))
+    queryset = ProdutoReceita.objects.filter(receita__in=receita)
     products = {}
+    total = decimal.Decimal(0)
     for obj in queryset:
         if not products.get(obj.produto.nome):
             products[obj.produto.nome] = {
@@ -69,11 +64,57 @@ def despesa(request):
             }
         products[obj.produto.nome]['quantidade'] += obj.quantidade
         products[obj.produto.nome]['total'] += (obj.valor * obj.quantidade)
+        total += (obj.valor * obj.quantidade)
+    return products, total
 
+
+def get_products_despesa_total(data_inicial, data_final):
+    despesa = Despesa.objects.filter(data__range=(data_inicial, data_final))
+    queryset = ProdutoDespesa.objects.filter(despesa__in=despesa)
+    products = {}
+    total = decimal.Decimal(0)
+    for obj in queryset:
+        if not products.get(obj.produto.nome):
+            products[obj.produto.nome] = {
+                'quantidade': decimal.Decimal(0),
+                'total': decimal.Decimal(0),
+                'data': None
+            }
+        products[obj.produto.nome]['quantidade'] += obj.quantidade
+        products[obj.produto.nome]['total'] += (obj.valor * obj.quantidade)
+        total += (obj.valor * obj.quantidade)
+    return products, total
+
+
+@login_required(login_url='/admin/login/')
+@permission_required('accounting.add_despesa', raise_exception=True)
+def index(request):
+    data_inicial, data_final = range_date(request)
+    despesas, _ = get_products_despesa_total(data_inicial, data_final)
+    receitas, _ = get_products_receita_total(data_inicial, data_final)
+    top_despesas = sorted(despesas.iteritems(), key=lambda (x, y): y['total'], reverse=True)[:5]
+    top_receitas = sorted(receitas.iteritems(), key=lambda (x, y): y['total'], reverse=True)[:5s]
+
+    centro = CentroDeCusto.objects.all()
+
+    context = {
+        'centro': centro,
+        'despesas': top_despesas,
+        'receitas': top_receitas
+    }
+    return render(request, 'accounting/index.html', context)
+
+
+@login_required(login_url='/admin/login/')
+@permission_required('accounting.add_despesa', raise_exception=True)
+def despesa(request):
+    data_inicial, data_final = range_date(request)
+    products, total = get_products_despesa_total(data_inicial, data_final)
     context = {
         'products': products,
         'data_inicial': data_inicial,
-        'data_final': data_final
+        'data_final': data_final,
+        'total': total
     }
     return render(request, 'accounting/despesa.html', context)
 
@@ -82,23 +123,12 @@ def despesa(request):
 @permission_required('accounting.add_despesa', raise_exception=True)
 def receita(request):
     data_inicial, data_final = range_date(request)
-    receita = Receita.objects.filter(data__range=(data_inicial, data_final))
-    queryset = ProdutoReceita.objects.filter(receita__in=receita)
-    products = {}
-    for obj in queryset:
-        if not products.get(obj.produto.nome):
-            products[obj.produto.nome] = {
-                'quantidade': decimal.Decimal(0),
-                'total': decimal.Decimal(0),
-                'data': None
-            }
-        products[obj.produto.nome]['quantidade'] += obj.quantidade
-        products[obj.produto.nome]['total'] += (obj.valor * obj.quantidade)
-
+    products, total = get_products_receita_total(data_inicial, data_final)
     context = {
         'products': products,
         'data_inicial': data_inicial,
-        'data_final': data_final
+        'data_final': data_final,
+        'total': total
     }
 
     return render(request, 'accounting/receita.html', context)
